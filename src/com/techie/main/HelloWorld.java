c86b22ee945423656ca022db2ac17e8223b6a5d8
Abhijeet <abjt.mukesh@gmail.com>
2017-04-13 00:09:32 +0530

    demo project testing

---
+++ src/com/techie/main/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.techie.main;
+
+public class HelloWorld {
+	
+	private HelloWorld() {
+	}
+
+	public static void main(String[] args) {
+		System.out.println("Hellow World....!!!!");
+	}
+
+}
